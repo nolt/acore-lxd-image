0b89f00589ea5193e85c0335cd6be171aba0d662
nolt <nolt@wizzi.pl>
2022-06-23 07:12:49 +0000

    small fix

--- acore-lxd.sh
+++ acore-lxd.sh
@@ -51,7 +51,6 @@ sleep 5
 lxc exec acoremariadb -- bash -c "
 apt update && apt full-upgrade -y
 wget https://downloads.mariadb.com/MariaDB/mariadb_repo_setup
-echo "d4e4635eeb79b0e96483bd70703209c63da55a236eadd7397f769ee434d92ca8 mariadb_repo_setup" | sha256sum -c -
 chmod +x mariadb_repo_setup
 ./mariadb_repo_setup --mariadb-server-version="mariadb-10.6"
 rm -f mariadb_repo_setup
